d4aaf5c974eee27b965901149c32ba54a5363386
namanmeghwal <75925647+namanmeghwal@users.noreply.github.com>
2022-10-18 14:31:12 +0530

    Update Shape-program.py

--- Shape-program.py
+++ Shape-program.py
@@ -1,13 +1,16 @@
 import turtle
+import matplotlib.pyplot as plt
 
 print("-------For Printing The Shape You Have To Put Number-------")
-print("For Creation Of Triangle Press [1]:--")
-print("For Creation Of Square Press   [2]:--")
-print("           TO Exit             [3]:--")
+print("For Creation Of Triangle shape [1]:--")
+print("For Creation Of Square shpae   [2]:--")
+print("For Creation Of Pentagon shape [3]:--")
+print("           TO Exit             [4]:--")
 a = int(input("Enter Number"))
 for j in a:
    if a==1:
       def tri(side):
+         #For Loop For creating The triagle shape of coding.
          for i in range(3):
             my_pen.fd(side)
             my_pen.left(120)
@@ -28,7 +31,8 @@ for j in a:
       for i in range(10):
          tri(side)
          side -= 30
-
+   
+   #for creating the shape of square the loop of the code helps in creating the shape of it.
    elif a==2:
       def squa(side):
          for i in range(4):
@@ -49,56 +53,17 @@ for j in a:
          squa(side)
          side-= 20
    
+   #to exit the line of code it ends with the code of line break.
    elif a==3:
+      sp = [[1,1], [2,1], [2,2], [1,2], [0.5,1.5]]
+      sp.append(sp[0]) 
+      s1, s2 = zip(sp)
+      plt.figure()
+      plt.plot(s1,s2) 
+      plt.show() 
+   elif a==4:
       break
    
    else:
       break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
 
